d26277fa46509701e6f572e3f3030f93032f5aac
Olya.Chistikova <olya.chistikova@mail.ru>
2026-03-23 08:18:54 +0300

    My_first_Commit

---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,20 @@
+package org.example;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+    Calculator calc = new Calculator();
+
+    @Test
+    public void addTest(){
+        assertEquals(5.0, calc.add(2, 3));
+    }
+
+    @Test
+    public void subtractTest(){
+        assertEquals(-1.0, calc.subtract(2, 3));
+    }
+}
